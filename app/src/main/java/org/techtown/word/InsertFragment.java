9d3a387aca6d16ffe4ab1eb44a48973cfb01dca4
박태광 <ptk725739@gmail.com>
2021-08-27 22:52:53 +0900

    21-08-27 fragment java 파일 수정 커밋

--- app/src/main/java/org/techtown/word/InsertFragment.java
+++ app/src/main/java/org/techtown/word/InsertFragment.java
@@ -11,9 +11,9 @@ import android.view.ViewGroup;
 public class InsertFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_insert, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_insert, container, false);
+
+        return view;
     }
 }
--- app/src/main/java/org/techtown/word/WordListFragment.java
+++ app/src/main/java/org/techtown/word/WordListFragment.java
@@ -11,9 +11,8 @@ import android.view.ViewGroup;
 public class WordListFragment extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_word_list, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_word_list, container, false);
+        return view;
     }
 }
