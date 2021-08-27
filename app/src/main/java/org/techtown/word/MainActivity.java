51a90e9dece563a47ee4ac63d137e4c8b0ee67a6
chrhfoq123 <chrhfoq123@naver.com>
2021-08-27 22:56:21 +0900

    21-08-27 뷰페이저 추가 커밋

--- app/src/main/java/org/techtown/word/MainActivity.java
+++ app/src/main/java/org/techtown/word/MainActivity.java
@@ -1,14 +1,21 @@
 package org.techtown.word;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    ViewPager pager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        pager = findViewById(R.id.pager);
+        pager.setOffscreenPageLimit(3);
+
+        PagerManager pagerManager = new PagerManager(getSupportFragmentManager());
     }
 }
---
+++ app/src/main/java/org/techtown/word/PagerManager.java
@@ -0,0 +1,31 @@
+package org.techtown.word;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+
+public class PagerManager extends FragmentStatePagerAdapter {
+
+    private ArrayList<Fragment> items = new ArrayList<Fragment>();
+
+    public PagerManager(FragmentManager fm){
+        super(fm);
+    }
+
+    public void addItem(Fragment item){
+        items.add(item);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return items.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+}
