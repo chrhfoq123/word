744d8faaa6b09cd4ff0b9582efa60a001299f126
박태광 <ptk725739@gmail.com>
2021-08-27 22:47:47 +0900

    21-08-27 입력, 리스트 출력 레이아웃 추가 커밋

---
+++ app/src/main/java/org/techtown/word/InsertFragment.java
@@ -0,0 +1,19 @@
+package org.techtown.word;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class InsertFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_insert, container, false);
+    }
+}
