1002c8a40674d37c0eafcb5697f65b236a23239a
chrhfoq123 <chrhfoq123@naver.com>
2021-08-27 23:33:02 +0900

    21-08-27 주석 추가 커밋

--- app/src/main/java/org/techtown/word/DAO.java
+++ app/src/main/java/org/techtown/word/DAO.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 
 import androidx.fragment.app.Fragment;
 
+//data access object
 public class DAO {
     private SQLiteDatabase db;
     private Context context;
